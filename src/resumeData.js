4fbb307fe2b9e5c4ec19979f14ae03017456f0d2
uvsq21919049 <ahmed.saadi@ens.uvsq.fr>
2020-06-16 16:12:42 +0200

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,144 @@
+let resumeData = {
+    "imagebaseurl":"https://ahmedglas.github.io/",
+    "name": "Saadi Ahmed",
+    "role": "Full Stack Developer and Data Scientist",
+    "linkedinId":" ahmed.esssaadi@gmail.com",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/saadi-ahmed-708148183/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/ahmedglas",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"facebook",
+          "url":"https://www.facebook.com/ahmed.essaadi",
+          "className":"fa fa-facebook"
+        },
+        {
+          "name":"stackoverflow",
+          "url":"https://stackoverflow.com/users/11378778/ahmed-essaadi",
+          "className":"fa fa-stack-overflow"
+        }
+      ],
+    "aboutme":"I am currently a final year student at the University of Versailles . I am a self taught Full Stack Developer, currently diving deeper into Machine Learning, Cloud computing and new technologies. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"France,Montrouge",
+    "website":"92120",
+    "education":[
+      {
+        "UniversityName":"University of Versailles",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"2019",
+        "YearOfPassing":"- 2020",
+        "Achievements":"created many project in many languages like C/C++/QT/Python"
+      },
+      {
+        "UniversityName":"ISAMM Higher Institute of Arts Manouba Multimedia",
+        "specialization":"Computer Science and Multimedia",
+        "MonthOfPassing":"2017",
+        "YearOfPassing":"- 2019",
+        "Achievements":"Graduated.                           participated in many hackathon events , clubs  ..."
+      },
+      {
+        "UniversityName":"High School 'Rue de Pacha'",
+        "specialization":"Bachlor of Science",
+        "MonthOfPassing":"2016",
+        "YearOfPassing":"",
+        "Achievements":"Baccalaureate S"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Digitalis Plus",
+        "specialization":"Full Stack Developer",
+        "MonthOfLeaving":"01/02/2019",
+        "YearOfLeaving":"- 15/05/2019",
+        "Achievements":"Creation of a web and mobile application using  'Frontend : REACTJS/REACT-NATIVE ; Backend : NODEJS / EXPRESSE ; Database : MONGODB' "
+      },
+      {
+        "CompanyName":"FDR COMMERCE",
+        "specialization":"Frontend developper",
+        "MonthOfLeaving":"01/07/2018",
+        "YearOfLeaving":"- 31/07/2018",
+        "Achievements":"Web app using HTML5 PHP CSS  LARAVEL"
+      },
+      {
+        "CompanyName":"University of Versailles",
+        "specialization":"python developper",
+        "MonthOfLeaving":"01/02/2020",
+        "YearOfLeaving":"- 25/05/2020",
+        "Achievements":"Turing machine simulator"
+      }
+    ],
+    "skillsDescription":"Some of my skills",
+    "skills":[
+      {
+        "skillname":"Reactjs / React Native"
+      },
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"nodejs / EXPRESSE"
+      },
+      {
+        "skillname":"php / laravel "
+      },
+      {
+        "skillname":"c / c++ / c#"
+      },
+      {
+        "skillname":"Python / qt"
+      },
+      {
+        "skillname":"photoshop / illustrator / maya3d"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"doctoury app",
+        "description":"web and mobile app created with ReactJs, React Native, Express, Nodejs, MongoDB",
+        "imgurl":"images/portfolio/mern1.jpg",
+        "url":"https://github.com/ahmedglas/doctoury-app"
+      },
+      {
+        "name":"Aircraft fuel  ",
+        "description":"Aircraft simulateur for fuel failure, created with c++ and Qt ",
+        "imgurl":"images/portfolio/qt1.jpg",
+        "url" :"https://github.com/ahmedglas/projet_c"
+      },
+      {
+        "name":"School project",
+        "description":"Ecommerce websit, created with HTML5, PHP, JS, SQL ",  
+        "imgurl":"images/portfolio/project1.jpg",
+        "url":"https://github.com/ahmedglas/siteweb"
+      },
+      {
+        "name":"Turing machine",
+        "description":"Turing machine simulateur, created with Python, PyQt",
+        "imgurl":"images/portfolio/py1.jpg",
+        "url":"https://github.com/ahmedglas/py-qt"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
